8694fb1505511f2dcf23e48679d7d707f6d4af30
meetsudani04-codebrain <meetsudani@codebraininfotech.com>
2025-12-25 12:40:59 +0530

    create new endpoint

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def subtract(a: int, b: int) -> int:
 def multiply(a: int, b: int) -> int:
     return a * b
 
+@app.get("/divide/{a}/{b}")
+def divide(a: int, b: int) -> float:    
+    if b == 0:
+        return {"error": "Division by zero is not allowed."}
+    return a / b
